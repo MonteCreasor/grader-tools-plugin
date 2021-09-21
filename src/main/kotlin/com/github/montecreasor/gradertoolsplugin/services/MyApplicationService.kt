cf1f6b118d49c3ed9438e4fee45855b11f0cb792
GitHub Action <action@github.com>
2021-09-21 23:45:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/montecreasor/gradertoolsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.montecreasor.gradertoolsplugin.services
+
+import com.github.montecreasor.gradertoolsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/montecreasor/gradertoolsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/montecreasor/gradertoolsplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.montecreasor.gradertoolsplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.montecreasor.gradertoolsplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
